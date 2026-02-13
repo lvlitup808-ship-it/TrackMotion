48c35ada72161ce61cd21e4cb3d886c1547809a2
lvlitup808-ship-it <lvlitup808@gmail.com>
2026-02-13 17:17:20 -0500

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,42 @@
+// swift-tools-version: 5.9
+import PackageDescription
+
+let package = Package(
+    name: "TrackMotion",
+    platforms: [
+        .iOS(.v17)
+    ],
+    products: [
+        .library(name: "TrackMotion", targets: ["TrackMotion"])
+    ],
+    dependencies: [
+        // TensorFlow Lite for pose estimation
+        .package(
+            url: "https://github.com/tensorflow/tensorflow",
+            .upToNextMajor(from: "2.14.0")
+        ),
+        // Swift Numerics for biomechanical calculations
+        .package(
+            url: "https://github.com/apple/swift-numerics",
+            from: "1.0.2"
+        ),
+        // Swift Algorithms for data processing
+        .package(
+            url: "https://github.com/apple/swift-algorithms",
+            from: "1.2.0"
+        )
+    ],
+    targets: [
+        .target(
+            name: "TrackMotion",
+            dependencies: [
+                .product(name: "RealModule", package: "swift-numerics"),
+                .product(name: "Algorithms", package: "swift-algorithms")
+            ],
+            path: "TrackMotion",
+            resources: [
+                .process("Resources")
+            ]
+        )
+    ]
+)
